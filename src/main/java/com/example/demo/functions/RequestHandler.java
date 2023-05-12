0e71b2afad2c594f9c6f039c6783363c8b66cfa5
O'Dwayne Irving <rudeyute@users.noreply.github.com>
2023-05-12 11:45:20 -0400

    Updated terraform configs to upload actual jar instead of packaging in zip. Also updated to utilize AWS Terraform Lambda module. Added sample Springboot app that has a request handler for APIGateway requests

---
+++ src/main/java/com/example/demo/functions/RequestHandler.java
@@ -0,0 +1,12 @@
+package com.example.demo.functions;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class RequestHandler implements com.amazonaws.services.lambda.runtime.RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Ay yoo!!!");
+    }
+}
